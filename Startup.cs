a1eb68cdfd80b23d20e06dd1615ef67c7c3a9711
LCANOASUS <livingstone23@gmail.com>
2021-11-03 08:28:01 +0100

    Initial Charge

---
+++ Startup.cs
@@ -0,0 +1,40 @@
+
+
+namespace Apicore6GQL;
+
+    public class Startup{
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }    
+
+        public void ConfigureServices(IServiceCollection services){
+            services.AddControllers();
+            services.AddSwaggerGen(c =>{
+                c.SwaggerDoc("v1", new() { Title = "Apicore6GQL", Version = "v1"});
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env){
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","Apicore6GQL v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>{
+                endpoints.MapControllers();
+            });
+
+        }
+        
+    }  
+
